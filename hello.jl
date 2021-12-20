69e7d7a34b2c5aeb5c9a605c22c09754cbf9ef2d
Avvisati Guido (GT MO RM) <guido.avvisati@enel.com>
2021-12-20 18:19:07 +0100

    links

--- hello.jl
+++ hello.jl
@@ -1,1 +1,21 @@
-print("Hello! This is TechyTok!")
+## this is a code cell
+# https://techytok.com/lesson-modules/
+println("Hello")
+println("This is TechyTok!")
+## this is another code cell
+println(42)
+##
+1//3*30
+function plus_two(x)
+    return x+2
+end
+plus_two(3*6)
+##
+"""
+sums first, second and keyword third and fourth argument
+"""
+function foo(a, b=2; c, d=3)
+    return a + b + c + d
+end
+foo(3, c=1)
+##
